65dd46350bced2c38b32058ee4c7d3cd3eba4b13
tya5 <tetsuyayasuda@gmail.com>
2012-06-29 17:49:51 +0900

    impl service description

--- src/org/tyas/upnp/Upnp.java
+++ src/org/tyas/upnp/Upnp.java
@@ -1,37 +1,5 @@
 package org.tyas.upnp;
 
-import java.util.Set;
-
 public class Upnp
 {
-	public interface Service
-	{
-		UpnpServiceType getType();
-		UpnpServiceId getId();
-		String getScpdUrl();
-		String getControlUrl();
-		String getEventSubUrl();
-	}
-
-	public interface Device
-	{
-		UpnpDeviceType getType();
-		String getFriendlyName();
-		String getManufacturer();
-		String getManufacturerUrl();
-		String getModelDescription();
-		String getModelName();
-		String getModelNumber();
-		String getModelUrl();
-		String getSerialNumber();
-		UpnpUdn getUdn();
-		String getUpc();
-		String getPresentationUrl();
-
-		Set<UpnpServiceId> getServiceSet();
-		Set<UpnpUdn> getDeviceSet();
-
-		Service getService(UpnpServiceId id);
-		Device getDevice(UpnpUdn udn);
-	}
 }
